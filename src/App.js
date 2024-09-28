2a819c442a0da44fc0cc957fcf93e75a0c874e32
achen1076 <achen1076@gmail.com>
2024-09-28 04:06:21 -0400

    <3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Logan and Cam should get back together
+        <br />
+        <br />
+        <br />
+        <img
+          src="https://media.licdn.com/dms/image/D5603AQFl7s4PwuuRvA/profile-displayphoto-shrink_200_200/0/1689787838327?e=2147483647&v=beta&t=00Lo7sulVOJKyLRKnIVP8VcvcUvXb8Wdn4qRWrVDhXw"
+          width="200px"
+          height="200px"
+        />
+        +
+        <img
+          src="https://p16-va.lemon8cdn.com/user-avatar-musically/167a9dbd63956200a68fca291dd3f57b~tplv-tej9nj120t-origin.webp"
+          width="200px"
+          height="200px"
+        />
+        =
+        <img
+          src="https://img.icons8.com/?size=100&id=DFU1kReSUccu&format=png&color=000000"
+          width="200px"
+          height="200px"
+        />{" "}
       </header>
     </div>
   );
